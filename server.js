5d989b867b31e57c42bd75e495ce9a124eb9b94b
wmcclung <41096349+wmcclung@users.noreply.github.com>
2019-01-27 22:17:50 -0600

    updating mongoose connection to work with heroku

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/Sk8Scraper", { useNewUrlParser: true });
+mongoose.connect("mongodb://user:password123@ds113855.mlab.com:13855/heroku_fc1z06jz", { useNewUrlParser: true });
 
 // Routes
 
