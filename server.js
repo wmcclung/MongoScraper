51a9d5f1e31e80620d38428b136d68d76d5bdee4
wmcclung <41096349+wmcclung@users.noreply.github.com>
2019-01-28 21:31:08 -0600

    removing new url parser from mongoose connection
    see if this helps with hosting on heroku

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://user:password123@ds113855.mlab.com:13855/heroku_fc1z06jz", { useNewUrlParser: true });
+mongoose.connect("mongodb://user:password123@ds113855.mlab.com:13855/heroku_fc1z06jz");
 
 // Routes
 
